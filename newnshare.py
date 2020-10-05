01e3da26c27c6e4225e3a69046acfa24c439ebb2
maithedong92 <72377981+maithedong92@users.noreply.github.com>
2020-10-05 15:29:35 +0700

    Update newnshare.py

--- newnshare.py
+++ newnshare.py
@@ -12,5 +12,8 @@ gc = gspread.service_account()
 sh = gc.create('A new spreadsheet')
 
 
-#chia se sheet voi acc chinh
+#chia se sheet voi acc chinh. kiem tra thu muc chia se trong ggdrive, co the su dung role owner
 sh.share('2susulucky@gmail.com', perm_type='user', role='writer')
+
+#in noi dung cua sheet1 o A1
+print(sh.sheet1.get('A1'))
